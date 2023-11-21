96161d9fcbf7bfe45c9fdb78d834efaae33baea4
Abdulkadirhaji1 <akdabo55@yahoo.com>
2023-11-21 13:42:24 -0600

    add get move function

--- player.py
+++ player.py
@@ -15,11 +15,28 @@ class RandoomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        # get a random valid spot for our next move
+        square = random.choice(game.available_moves())
+        return square
 
 class HummanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (-9):')
+            # we're going to check that this is a correct value by trying to cast
+            # it to an integer, and if it's not, then we say its invalid
+            # if that spot is not available on the board, we also say its invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True # if thse are succefull, then yay!
+            except ValueError:
+                print('Invalid square. Try again.')
+
+            return val
